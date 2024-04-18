4e825990eae1ea3efac899e3622326363ae0ea25
tdeborde <33256624+tristandeborde@users.noreply.github.com>
2024-04-18 10:21:36 +0200

    Update gigax.lua

--- gigax/gigax.lua
+++ gigax/gigax.lua
@@ -1,13 +1,8 @@
-----------------------
---module "gigax"
-----------------------
-
 --server--------------
 ----------------------
 mod = {}
-gigaxServer = {}
 
-gigaxServer._init = function(self)
+mod._init = function(self)
     -- Initialize tables to hold NPC and location data
     print("Initializing Gigax server...")
     self._engineId = nil
@@ -17,15 +12,15 @@ gigaxServer._init = function(self)
     self._locationData = {}
 end
 
-gigaxServer.receiveEvent = function (self, e)
+mod.receiveEvent = function (self, e)
     if e.action == "registerNPC" then
-        gigaxServer:_registerNPC(e.avatarId, e.physicalDescription, e.psychologicalProfile, e.currentLocationName)
+        mod:_registerNPC(e.avatarId, e.physicalDescription, e.psychologicalProfile, e.currentLocationName)
     elseif e.action == "registerLocation" then
-        gigaxServer:_registerLocation(e.name, e.position, e.description)
+        mod:_registerLocation(e.name, e.position, e.description)
     elseif e.action == "registerEngine" then
-        gigaxServer:_registerEngine(e.Sender)
+        mod:_registerEngine(e.Sender)
     elseif e.action == "stepMainCharacter" then
-        gigaxServer:_stepMainCharacter(self._character, self._engineId, e.actionType, self._npcData["aduermael"]._id, self._npcData["aduermael"].name, e.content)
+        mod:_stepMainCharacter(self._character, self._engineId, e.actionType, self._npcData["aduermael"]._id, self._npcData["aduermael"].name, e.content)
     elseif e.action == "updateCharacterLocation" then
         local closest = _gigaxClient:findClosestLocation(e.position, self._locationData)
         -- if closest._id is different from the current location, update the character's location
@@ -34,7 +29,7 @@ gigaxServer.receiveEvent = function (self, e)
             return
         end
         if closest._id ~= self._character.current_location._id then
-            gigaxServer:_updateCharacterLocation(self._engineId, e.characterId, closest._id)
+            mod:_updateCharacterLocation(self._engineId, e.characterId, closest._id)
         end
     else
         print("Unknown Gigax message received from server.")
@@ -42,7 +37,7 @@ gigaxServer.receiveEvent = function (self, e)
 end
 
 -- Function to create and register an NPC
-gigaxServer._registerNPC = function(self, avatarId, physicalDescription, psychologicalProfile, currentLocationName)
+mod._registerNPC = function(self, avatarId, physicalDescription, psychologicalProfile, currentLocationName)
     -- Add NPC to npcData table
     self._npcData[avatarId] = {
         name = avatarId,
@@ -79,7 +74,7 @@ gigaxServer._registerNPC = function(self, avatarId, physicalDescription, psychol
 end
 
 -- Function to register a location
-gigaxServer._registerLocation = function(self, name, position, description)
+mod._registerLocation = function(self, name, position, description)
     self._locationData[name] = {
         position = {x = position._x, y = position._y, z = position._z},
         name = name,
@@ -87,7 +82,7 @@ gigaxServer._registerLocation = function(self, name, position, description)
     }
 end
 
-gigaxServer._registerEngine = function(self, sender)
+mod._registerEngine = function(self, sender)
     local apiUrl = API_URL .. "/api/engine/company/"
     local headers = {
         ["Content-Type"] = "application/json",
@@ -155,7 +150,7 @@ gigaxServer._registerEngine = function(self, sender)
     end)
 end
 
-gigaxServer._registerMainCharacter = function(self, engineId, locationId, sender)
+mod._registerMainCharacter = function(self, engineId, locationId, sender)
     -- Example character data, replace with actual data as needed
     local newCharacterData = {
         name = "oncheman",
@@ -179,7 +174,7 @@ gigaxServer._registerMainCharacter = function(self, engineId, locationId, sender
         if response.StatusCode ~= 200 then
             print("Error creating or fetching main character: " .. response.StatusCode)
         end
-        gigaxServer._character = JSON:Decode(response.Body)
+        mod._character = JSON:Decode(response.Body)
         local e = Event()
         e.action = "mainCharacterCreated"
         e["character"] = self._character
@@ -187,7 +182,7 @@ gigaxServer._registerMainCharacter = function(self, engineId, locationId, sender
     end)
 end
 
-gigaxServer._stepMainCharacter = function(self, character, engineId, actionType, targetId, targetName, content)
+mod._stepMainCharacter = function(self, character, engineId, actionType, targetId, targetName, content)
     -- Now, step the character
     local stepUrl = API_URL .. "/api/character/" .. character._id .. "/step-no-ws?engine_id=" .. engineId 
     local stepActionData = {
@@ -232,7 +227,7 @@ gigaxServer._stepMainCharacter = function(self, character, engineId, actionType,
     end)
 end
 
-gigaxServer._updateCharacterLocation = function(self, engineId, characterId, locationId)
+mod._updateCharacterLocation = function(self, engineId, characterId, locationId)
     local updateData = {
         -- Fill with necessary character update information
         current_location_id = locationId
@@ -256,160 +251,4 @@ gigaxServer._updateCharacterLocation = function(self, engineId, characterId, loc
     end)
 end
 
-
---client--------------
-----------------------
-
-_gigaxClient = {}
-
-_gigaxClient.init = function(self)
-    self.npcDataClient = {}
-end
-
-_gigaxClient.receiveClientEvent = function(self, e)
-    if e.action == "NPCActionResponse" then
-        _gigaxClient:parseAction(e)
-    elseif e.action == "mainCharacterCreated" then
-        -- Setup a new timer to delay the next update call
-        characterId = e.character._id
-        updateLocationTimer = Timer(0.5, true, function()
-            local e = Event()
-            e.action = "updateCharacterLocation"
-            e.position = Player.Position
-            e.characterId = characterId
-            e:SendTo(Server)
-        end)
-        -- print("Character ID: " .. character._id)
-    elseif e.action == "NPCRegistered" then
-        -- Update NPC in the client side table to add the _id
-        for _, npc in pairs(self.npcDataClient) do
-            if npc.name == e.npcName then
-                npc._id = e.npcId
-            end
-        end
-    end
-end
-
-_gigaxClient.createNPC = function(_, avatarId, physicalDescription, psychologicalProfile, currentLocationName, currentPosition)
-    -- Create the NPC's Object and Avatar
-    local NPC = {}
-    NPC.object = Object()
-    World:AddChild(NPC.object)
-    NPC.object.Position = currentPosition or Number3(0, 0, 0)
-    NPC.object.Scale = 0.5
-    NPC.object.Physics = PhysicsMode.Trigger
-    NPC.object.CollisionBox = Box({
-		-TRIGGER_AREA_SIZE.Width * 0.5,
-		math.min(-TRIGGER_AREA_SIZE.Height, NPC.object.CollisionBox.Min.Y),
-		-TRIGGER_AREA_SIZE.Depth * 0.5,
-	}, {
-		TRIGGER_AREA_SIZE.Width * 0.5,
-		math.max(TRIGGER_AREA_SIZE.Height, NPC.object.CollisionBox.Max.Y),
-		TRIGGER_AREA_SIZE.Depth * 0.5,
-	})
-    NPC.object.OnCollisionBegin = function(self, other)
-        if other ~= Player then
-            return
-        end
-        _helpers.lookAt(self.avatarContainer, other)
-    end
-    NPC.object.OnCollisionEnd = function(self, other)
-        if other ~= Player then
-            return
-        end
-        _helpers.lookAt(self.avatarContainer, nil)
-    end
-
-    local container = Object()
-	container.Rotation = NPC.object.Rotation
-	container.initialRotation = NPC.object.Rotation:Copy()
-	container.initialForward = NPC.object.Forward:Copy()
-	container:SetParent(NPC.object)
-    container.Physics = PhysicsMode.Dynamic
-	NPC.object.avatarContainer = container
-    
-    local avatar = require("avatar")
-    NPC.avatar = avatar:get(avatarId)
-    NPC.avatar:SetParent(NPC.object.avatarContainer)
-
-    -- Register it
-    local e = Event()
-    e.action = "registerNPC"
-    e.avatarId = avatarId
-    e.physicalDescription = physicalDescription
-    e.psychologicalProfile = psychologicalProfile
-    e.currentLocationName = currentLocationName
-    e:SendTo(Server)
-    return NPC
-end
-
-_gigaxClient.createLocation = function(_, name, position, description)
-    local e = Event()
-    e.action = "registerLocation"
-    e.name = name
-    e.position = position
-    e.description = description
-    e:SendTo(Server)
-end
-
-_gigaxClient.findNPCById = function(self, id)
-    for _, npc in pairs(self.npcDataClient) do
-        -- print("Checking NPC with ID " .. npc._id)
-        print("Looking for NPC with ID " .. id)
-        if npc._id == id then
-            return npc
-        end
-    end
-end
-
-_gigaxClient.parseAction = function(self, action)
-    local npc = self:findNPCById(action.protagonistId)
-    if action.actionType == "GREET" then
-        -- TODO: face action.target and wave hand
-        dialog:create("<Greets you warmly!>", npc.avatar)
-        npc.avatar.Animations.SwingRight:Play()
-    elseif action.actionType == "SAY" then
-        dialog:create(action.content, npc.avatar)
-    elseif action.actionType == "JUMP" then
-        dialog:create("<Jumps in the air!>", npc.avatar)
-        npc.object.avatarContainer.Velocity.Y = 50
-        timer = Timer(1, false, function()
-            npc.object.avatarContainer.Velocity.Y = 50
-        end)
-    elseif action.actionType == "MOVE" then
-        -- TODO
-    elseif action.actionType == "FOLLOW" then
-        -- TODO
-    end
-end
-
--- Function to calculate distance between two positions
-_gigaxClient.calculateDistance = function(_, pos1, pos2)
-    local dx = pos1.X - pos2.x
-    local dy = pos1.Y - pos2.y
-    local dz = pos1.Z - pos2.z
-    return math.sqrt(dx*dx + dy*dy + dz*dz)
-end
-
-_gigaxClient.findClosestLocation = function (self, playerPosition, locationData)
-    -- Assume `playerPosition` holds the current position of the player
-    local closestLocation = nil
-    local smallestDistance = math.huge -- Large initial value
-    
-    for _, location in pairs(locationData) do
-        local distance = self:calculateDistance(playerPosition, location.position)
-        if distance < smallestDistance then
-            smallestDistance = distance
-            closestLocation = location
-        end
-    end
-    
-    if closestLocation then
-        -- Closest location found, now send its ID to update the character's location
-        return closestLocation
-    end
-end
-
-mod.gigaxServer = gigaxServer
-mod.gigaxClient = _gigaxClient
 return mod
